df158d75421c7b824d25d93db2aeb6062ba32c90
David Amegah <amegah40@gmail.com>
2021-10-08 13:48:37 +0000

    Update with data binding

--- app/src/main/java/com/ommema/aboutme/MainActivity.kt
+++ app/src/main/java/com/ommema/aboutme/MainActivity.kt
@@ -9,30 +9,32 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import com.ommema.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-//        val showName : TextView = findViewById(R.id.show_name)
-//        showName.visibility = View.VISIBLE
 
-        val doneButton : Button = findViewById(R.id.done_button)
-        doneButton.setOnClickListener {
+        binding.doneButton.setOnClickListener {
 //            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
             showNickname()
         }
     }
 
     private fun showNickname() {
-        val nicknameText : EditText = findViewById(R.id.enter_nickname)
-        val showName : TextView = findViewById(R.id.show_name)
-        val userNickname = nicknameText.text
 
-        showName.text = userNickname.toString()
-        showName.visibility = View.VISIBLE
-        nicknameText.visibility = View.GONE
+        binding.apply {
+            showName.text = enterNickname.text.toString()
+
+            showName.visibility = View.VISIBLE
+//            nicknameText.visibility = View.GONE
+            enterNickname.visibility = View.GONE
+        }
 
     }
 }
